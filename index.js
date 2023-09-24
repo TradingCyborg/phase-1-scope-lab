7fa77b5a915adef021126a7acb77f1be31f9016c
Daniel Kinyua <my.github.kinyuad29@gmail.com>
2023-09-24 13:54:03 +0300

    Failing to bring tests to the greenzone despite fulfilling the requests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "John Doe";
+function uppercaseCustomerName() {
+    if (typeof customerName === 'string') {
+      customerName = customerName.toUpperCase();
+    }
+  }
+  uppercaseCustomerName();
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  // Call the function to set the global variable
+  setBestCustomer();
+  
